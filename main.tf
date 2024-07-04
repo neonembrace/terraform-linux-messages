3429c738e3528a505676effb1e296547b81331c8
neonembrace <github-neonembrace-to1-terraform@was.dev.mgm-tp.com>
2024-07-04 16:14:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "default" {
   provisioner "local-exec" {
-    command = "echo 'This is a test for the takeover.' && echo $(date)"
+    command = "echo 'This is a test for the takeover with a new webhook.' && echo $(date)"
   }
 }
